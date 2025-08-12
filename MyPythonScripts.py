36a199f7c8147fae5f58140f13578df521e94362
greyfox80 <greyfox80@gmail.com>
2025-08-12 07:23:06 +0700

    Update MyPythonScripts.py

--- MyPythonScripts.py
+++ MyPythonScripts.py
@@ -6,6 +6,10 @@ from keras.models import Sequential
 from keras.models import load_model
 #from tensorflow.python.keras.layers import Dense
 
+# Title
+st.title("Selamat Datang di Aplikasi Sorta - Sorting Trash Assistant")
+
+
 
 
 
